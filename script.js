1cddb731ef81cc9fe6b9a53feb433c335ed4f314
Christian Benitez <10christian@live.com>
2021-12-03 06:35:37 -0800

    Finished computer play function

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let computerSelection = ""
-function conputerPlay(){ //This will be how the computer selects either Rock, Paper, Scissors
-    number = Math.floor(Math.random()*3 +1);
-    if (number = 1){
+
+function computerPlay(){                            // This will be how the computer selects either Rock, Paper, Scissors
+    let rng = Math.floor(Math.random()*3+1);        // Chosing  a random number between 1 and 3
+    if (rng == 1){                                  // Then assigning number to a value 1 = Rock, 2 = Paper, 3 = Scissors
         computerSelection = "Rock";
-    } else if (number = 2){
+    } else if (rng == 2){
         computerSelection = "Paper";
-    }else {
+    } else if (rng == 3){
         computerSelection = "Scissors";
     }
     return computerSelection
-    console.log(computerSelection)
 }
 
 function playRound(playerSelection, computerSelection){ // This will take input from Player and Computer and decide winner
@@ -17,5 +16,4 @@ function playRound(playerSelection, computerSelection){ // This will take input 
 }
 
 function game(){  // This will loop the game to a best of 5
-
 }
