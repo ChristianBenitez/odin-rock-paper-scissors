9a946d04c74f46fa605ca28bb99e019a72b20ef3
Christian Benitez <10christian@live.com>
2021-12-11 20:36:02 -0800

    Repeat game till someone reaches 5 wins

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let computerSelection = ""
+let playerScore = 0
+let computerScore = 0
 function computerPlay(){                            // This will be how the computer selects either Rock, Paper, Scissors
     let rng = Math.floor(Math.random()*3+1);        // Chosing  a random number between 1 and 3
     if (rng == 1){                                  // Then assigning number to a value 1 = Rock, 2 = Paper, 3 = Scissors
@@ -19,15 +21,24 @@ function playRound(){ // This will take input from Player and Computer and decid
         || playerSelection == "paper" && computerSelection == "scissors" 
         || playerSelection == "scissors" && computerSelection == "rock"){
             console.log("You Lose");
+            computerScore++;
     } else if (playerSelection == "paper" && computerSelection == "rock" 
         || playerSelection == "scissors" && computerSelection == "paper" 
         || playerSelection == "rock" && computerSelection == "scissors"){
             console.log("You Win")
+            playerScore++;
     }
 }
 
-function game(){  // This will loop the game 5 time
-    for (let i =0; i < 5; i++){
+function game(){  // This will loop the game till one of you reaches 5 wins
+    let i = 5
+    if (i > playerScore && i > computerScore){
         playRound()
+        console.log(`Player ${playerScore} - Computer ${computerScore}`)
+        game()                        // Call itself again to repeat the game
+    } else if (i == playerScore){
+        console.log("You beat the machine!")
+    } else{
+        console.log("Better luck next time!")
     }
 }
